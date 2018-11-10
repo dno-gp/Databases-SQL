b881d76ec139b1c29a5d29217bcf3689aac1494a
Edson Sales <41214638+dno-gp@users.noreply.github.com>
2018-11-10 18:08:41 -0300

    Update fpm-mrsape.sql

--- fpm-mrsape.sql
+++ fpm-mrsape.sql
@@ -1,10 +1,11 @@
 /****************************
  **FPM-Microrregião Sapé-PB**
  ****************************/
--- 
-
+ 
+ -- Criando a base de dados
 create database fpmmrsape /*nome do banco*/;
--- Selecionando o banco de dados 
+
+-- Selecionando a base 
 use fpmmrsape;
 
 -- Criando as tabelas
@@ -25,14 +26,13 @@ valor decimal(10,2),
 foreign key (municipio) references municipios(idmunic)
 )charset = 'utf8';
 
--- Adicionando os municípios - Microrregião Sapé
+-- Adicionando os municípios da Microrregião Sapé-PB
 insert into municipios (municipio) 
 values("Cruz do Espírito Santo"), ("Juripiranga"),("Mari"),
 ("Pilar"), ("Riachão do Poço"), ("São José dos Ramos"), 
 ("São Miguel de Taipu"), ("Sapé"), ("Sobrado");
 
--- Importando dados a partir de um arquivo - Transferências Constitucionais obrigatórias
--- Arquivo baixado do site do Tesouro Nacional
+-- Importando dados a partir de um arquivo (baixado do site Tesouro Nacional) - Transferências Constitucionais obrigatórias
 load data infile '/var/lib/mysql-files/transferencias.csv' into table transferencias
 fields terminated by ';' enclosed by '"'
 ignore 1 lines
