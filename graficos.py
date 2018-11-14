f14b8de552ee67b8843131e943ca5c72fe960702
dno-gp <dno.sales@gmail.com>
2018-11-14 10:59:49 -0300

    Update

--- graficos.py
+++ graficos.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 #Conexão com o banco de dados
 try:
-    link = pymysql.connect(host="127.0.0.1", user="root", password="654321", db="fpmmrsape")
+    link = pymysql.connect(host="127.0.0.1", user="", password="", db="fpmmrsape")
 
 except:
     print("Conexão não estabelecida com a base de dados.")
@@ -21,28 +21,67 @@ try:
         c.execute(sqlano)
         ano = c.fetchall()
 
-        #Consulta municipios em loop
-        for i in range(1,10):
-            sql1 = 'select sum(valor) from transferencias where municipio = %s'
-            c.execute(sql1, (i))
-             
+        sql1 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (1))
+        ces = c.fetchall()
+            
+        sql2 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (2))
+        jpr = c.fetchall()
+        
+        sql3 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (3))
+        mri = c.fetchall()
+
+        sql4 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (4))
+        plr = c.fetchall()
+
+        sql5 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (5))
+        rip = c.fetchall()
+
+        sql6 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (6))
+        sjr = c.fetchall()
+
+        sql7 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (7))
+        smt = c.fetchall()
+
+        sql8 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (8))
+        spe = c.fetchall()
+
+        sql9 = "select sum(valor) from transferencias where municipio = %s group by ano order by ano"
+        c.execute(sql1, (9))
+        sbd = c.fetchall()
 except:
     print("Erro de consulta.")
 
 #Montando os gráficos
 try:
-    plt.plot(ano, valor, label ='Total de transferencias recebidas na região')
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2,1,1)
+    ax1 = plt.plot(ano, valor)
+
+    ax2 = fig.add_subplot(2,1,2)
+    ax2 = plt.plot(ano, ces, label = 'Cruz E. Santo')
+    ax2 = plt.plot(ano, jpr, label = 'Juripianga')
+    ax2 = plt.plot(ano, mri, label = 'Mari')
+    ax2 = plt.plot(ano, plr, label = 'Pilar')
+    ax2 = plt.plot(ano, rip, label = 'R. do Poço')
+    ax2 = plt.plot(ano, sjr, label = 'São J. Ramos')
+    ax2 = plt.plot(ano, smt, label = 'São M. Taipu')
+    ax2 = plt.plot(ano, spe, label = 'Sapé')
+    ax2 = plt.plot(ano, sbd, label = 'Sobrado')
+
     plt.legend()
-    plt.title("Transferencias")
     plt.show()
-    '''
-    fig =  plt.figure()
-    ax1= fig.add_subplot(2,1,1)
-    ax1.plot(ano,valor, label='Total de tansferencias para a região')
-    '''
+    
 except:
     print("Erro na criação dos gráficos")
 
 finally:
     link.close()
-    print("Fim do programa")
+    print("Saindo...")
